9e0df6c71d47e2e6994ff1c221bedd7710daab80
OKuzminova <yagodka029@yandex.ru>
2020-05-20 20:31:52 +0300

    Content pravki, 2 page

---
+++ form-reg.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <title>Anastasiiiashpak - Форма регистрации</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	      <!--РЕГИСТРАЦИЯ-->
+      <div class="block-6">
+        <div class="block-6-content">
+        	<div class="bloc-6-txt">
+
+					<form method="post" action="index.php">
+					<table class="forma">
+						<tr>
+							<td colspan="2"><a name="reg-form"></a><h2>РЕГИСТРАЦИЯ</h2></td>
+						</tr>
+						<tr>
+							<td><p>Ф.И.О.</p></td><!--__Поле таблицы №1___-->
+							<td><input type="text" name="name" placeholder="Введите имя" class="one"></td>
+						</tr>
+						<tr>
+							<td align="left"><p>Ваш инстаграм</p></td><!--__Поле таблицы №2___-->
+							<td><input type="text" name="inst" placeholder="Введите никнейм" class="one" class="inpt"></td>
+						</tr>
+						<tr>
+							<td><p>Телефон</p></td><!--__Поле таблицы №3___-->
+							<td><input type="tel" name="tel" placeholder="+7 (978) 000-00-00" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}"></td>
+						</tr>
+						<tr>
+							<td><p>Почта</p></td><!--__Поле таблицы №4___-->
+							<td><input type="text" name="email" placeholder="E-mail" class="one"></td>
+						</tr>
+						<tr>
+							<td colspan="2"><span>Если вы согласны на обрботку ваших персональных данных нажмите кнопку "Оплатить"</span>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2">
+								<p class="btn-3"><a href="https://money.yandex.ru/to/410018753700186" title="Оплатить" target="_blank">Оплатить | 990 ₽</a></p>
+							</td>
+						</tr>
+					</table>
+				</form>
+        	</div>
+        </div>
+      </div>
+      <!--END РЕГИСТРАЦИЯ -->
+</body>
+</html>
